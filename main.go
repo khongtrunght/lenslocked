1c5c77d22bb0e87e22c12d411aef5602f8bd6d27
KHONG TRUNG HT <khongtrunght@gmail.com>
2024-11-13 16:58:45 +0700

    Styled the signup form

--- main.go
+++ main.go
@@ -50,6 +50,9 @@ func main() {
 	tpl = views.Must(views.ParseFS(templates.FS, "tailwind.gohtml", "faq.gohtml"))
 	r.Get("/faq", controllers.FAQ(tpl))
 
+	tpl = views.Must(views.ParseFS(templates.FS, "tailwind.gohtml", "signup.gohtml"))
+	r.Get("/signup", controllers.StaticHandler(tpl))
+
 	fmt.Println("Server is running on port 3000")
 	http.ListenAndServe(":3000", r)
 }
