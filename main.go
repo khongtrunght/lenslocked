671daee6f5848a3ec4f99b5204b41d35593e0595
KHONG TRUNG HT <khongtrunght@gmail.com>
2024-11-23 08:12:26 +0700

    add gallery handler

--- main.go
+++ main.go
@@ -101,8 +101,10 @@ func main() {
 		SessionService: sessionService,
 	}
 
-	csrfMw := csrf.Protect([]byte(cfg.CSRF.Key), csrf.Secure(cfg.CSRF.Secure)) // TODO: set to true in production
-
+	csrfMw := csrf.Protect(
+		[]byte(cfg.CSRF.Key),
+		csrf.Secure(cfg.CSRF.Secure), // TODO: set to true in production
+		csrf.Path("/"))
 	// Setup controllers
 	usersC := controllers.Users{
 		UserService:          userService,
@@ -142,7 +144,13 @@ func main() {
 		r.Use(umw.RequireUser)
 		r.Get("/", usersC.CurrentUser)
 	})
-	r.Get("/galleries/new", galleriesC.New)
+	r.Route("/galleries", func(r chi.Router) {
+		r.Group(func(r chi.Router) {
+			r.Use(umw.RequireUser)
+			r.Get("/new", galleriesC.New)
+			r.Post("/", galleriesC.Create)
+		})
+	})
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
